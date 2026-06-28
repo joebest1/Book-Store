0894c9cb7e5dca9a849edc22f8ee22f5b49a3718
Yousef Ashraf <yousefashrafnadi@gmail.com>
2026-06-29 02:49:41 +0300

    Update AccountController and authentication features

--- obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0af19d783f75fa9ee1225211261703dccef68f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e86d707acde2d146af142481f917f780f819559")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
